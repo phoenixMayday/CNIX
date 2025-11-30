55c8fc0b3423d5f0351be2654965530acb535a9c
phoenixMayday <phoenixMayday0@gmail.com>
2025-11-30 18:39:42 +0000

    feat: refactor parser to have a ctx struct like generator

--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,11 @@
 #include "lexer.c"
 
+typedef struct {
+    Token *tokens;
+    int current_pos;
+    int token_count;
+} ParserCtx;
+
 // Terms
 typedef enum {
     NODE_TERM_INT_LIT,
@@ -93,27 +99,27 @@ typedef struct {
 } NodeProg;
 
 
-NodeTerm *parse_term(Token **tokens, int *token_pos, int token_count) {
-    Token *current = &(*tokens)[*token_pos];
-    if (current->type == TOKEN_INT_LIT) {
+NodeTerm *parse_term(ParserCtx *ctx) {
+    Token *peek = &ctx->tokens[ctx->current_pos];
+    if (peek->type == TOKEN_INT_LIT) {
         NodeTermIntLit *int_lit_node = malloc(sizeof(NodeTermIntLit));
-        int_lit_node->int_lit = *current;
+        int_lit_node->int_lit = *peek;
 
         NodeTerm *term_node = malloc(sizeof(NodeTerm));
         term_node->kind = NODE_TERM_INT_LIT;
         term_node->as.int_lit = int_lit_node;
 
-        (*token_pos)++;
+        ctx->current_pos++;
         return term_node;
-    } else if (current->type == TOKEN_IDENT) {
+    } else if (peek->type == TOKEN_IDENT) {
         NodeTermIdent *ident_node = malloc(sizeof(NodeTermIdent));
-        ident_node->ident = *current;
+        ident_node->ident = *peek;
         
         NodeTerm *term_node = malloc(sizeof(NodeTerm));
         term_node->kind = NODE_TERM_IDENT;
         term_node->as.ident = ident_node;
 
-        (*token_pos)++;
+        ctx->current_pos++;
         return term_node;
     }
     else {
@@ -122,19 +128,19 @@ NodeTerm *parse_term(Token **tokens, int *token_pos, int token_count) {
     }
 }
 
-NodeExpr *parse_expr(Token **tokens, int *token_pos, int token_count) {
+NodeExpr *parse_expr(ParserCtx *ctx) {
     // eval left term
-    NodeTerm *lhs_term = parse_term(tokens, token_pos, token_count);
+    NodeTerm *lhs_term = parse_term(ctx);
     NodeExpr *lhs_expr = malloc(sizeof(NodeExpr));
     lhs_expr->kind = NODE_EXPR_TERM;
     lhs_expr->as.term = lhs_term;
     
     // end of token stream
-    if (*token_pos >= token_count) {
+    if (ctx->current_pos >= ctx->token_count) {
         return lhs_expr;
     }
 
-    Token *peek = &(*tokens)[*token_pos];
+    Token *peek = &ctx->tokens[ctx->current_pos];
     if (peek->type != TOKEN_PLUS &&
         peek->type != TOKEN_MINUS &&
         peek->type != TOKEN_MUL &&
@@ -143,32 +149,32 @@ NodeExpr *parse_expr(Token **tokens, int *token_pos, int token_count) {
     }
 
     // eval operator
-    Token *op = &(*tokens)[*token_pos];
-    (*token_pos)++;
+    Token *op = &ctx->tokens[ctx->current_pos];
+    ctx->current_pos++;
 
     NodeExpr *result_expr = malloc(sizeof(NodeExpr));
     if (op->type == TOKEN_PLUS) {
         NodeExprAdd *add_node = malloc(sizeof(NodeExprAdd));
         add_node->lhs = lhs_expr;
-        add_node->rhs = parse_expr(tokens, token_pos, token_count);
+        add_node->rhs = parse_expr(ctx);
         result_expr->kind = NODE_EXPR_ADD;
         result_expr->as.add = add_node;
     } else if (op->type == TOKEN_MINUS) {
         NodeExprSub *sub_node = malloc(sizeof(NodeExprSub));
         sub_node->lhs = lhs_expr;
-        sub_node->rhs = parse_expr(tokens, token_pos, token_count);
+        sub_node->rhs = parse_expr(ctx);
         result_expr->kind = NODE_EXPR_SUB;
         result_expr->as.sub = sub_node;
     } else if (op->type == TOKEN_MUL) {
         NodeExprMul *mul_node = malloc(sizeof(NodeExprMul));
         mul_node->lhs = lhs_expr;
-        mul_node->rhs = parse_expr(tokens, token_pos, token_count);
+        mul_node->rhs = parse_expr(ctx);
         result_expr->kind = NODE_EXPR_MUL;
         result_expr->as.mul = mul_node;
     } else if (op->type == TOKEN_DIV) {
         NodeExprDiv *div_node = malloc(sizeof(NodeExprDiv));
         div_node->lhs = lhs_expr;
-        div_node->rhs = parse_expr(tokens, token_pos, token_count);
+        div_node->rhs = parse_expr(ctx);
         result_expr->kind = NODE_EXPR_DIV;
         result_expr->as.div = div_node;
     } else {
@@ -179,20 +185,20 @@ NodeExpr *parse_expr(Token **tokens, int *token_pos, int token_count) {
     return result_expr;
 }
 
-NodeStmt *parse_stmt(Token **tokens, int *token_pos, int token_count) {
-    Token *current = &(*tokens)[*token_pos];
+NodeStmt *parse_stmt(ParserCtx *ctx) {
+    Token *current = &ctx->tokens[ctx->current_pos];
     if (current->type == TOKEN_EXIT) {
-        (*token_pos)++;
+        ctx->current_pos++;
 
         // expect expression
-        NodeExpr *expr = parse_expr(tokens, token_pos, token_count);
+        NodeExpr *expr = parse_expr(ctx);
 
         // expect semicolon
-        if (*token_pos >= token_count || (*tokens)[*token_pos].type != TOKEN_SEMI) {
+        if (ctx->current_pos >= ctx->token_count || ctx->tokens[ctx->current_pos].type != TOKEN_SEMI) {
             fprintf(stderr, "Expected ';' at end of statement\n");
             exit(EXIT_FAILURE);
         }
-        (*token_pos)++;
+        ctx->current_pos++;
 
         NodeStmtExit *stmt_exit = malloc(sizeof(NodeStmtExit));
         stmt_exit->expr = expr;
@@ -203,32 +209,32 @@ NodeStmt *parse_stmt(Token **tokens, int *token_pos, int token_count) {
 
         return stmt_node;
     } else if (current->type == TOKEN_VAR) {
-        (*token_pos)++;
-        Token ident_token = (*tokens)[*token_pos];
+        ctx->current_pos++;
+        Token ident_token = ctx->tokens[ctx->current_pos];
 
         // expect identifier
-        if (*token_pos >= token_count || (*tokens)[*token_pos].type != TOKEN_IDENT) {
+        if (ctx->current_pos >= ctx->token_count || ctx->tokens[ctx->current_pos].type != TOKEN_IDENT) {
             fprintf(stderr, "Expected identifier after 'var'\n");
             exit(EXIT_FAILURE);
         }
-        (*token_pos)++;
+        ctx->current_pos++;
 
         // expect equals
-        if (*token_pos >= token_count || (*tokens)[*token_pos].type != TOKEN_EQUALS) {
+        if (ctx->current_pos >= ctx->token_count || ctx->tokens[ctx->current_pos].type != TOKEN_EQUALS) {
             fprintf(stderr, "Expected '=' after variable name\n");
             exit(EXIT_FAILURE);
         }
-        (*token_pos)++;
+        ctx->current_pos++;
 
         // expect expression
-        NodeExpr *expr = parse_expr(tokens, token_pos, token_count);
+        NodeExpr *expr = parse_expr(ctx);
 
         // expect semicolon
-        if (*token_pos >= token_count || (*tokens)[*token_pos].type != TOKEN_SEMI) {
+        if (ctx->current_pos >= ctx->token_count || ctx->tokens[ctx->current_pos].type != TOKEN_SEMI) {
             fprintf(stderr, "Expected ';' at end of statement\n");
             exit(EXIT_FAILURE);
         }
-        (*token_pos)++;
+        ctx->current_pos++;
 
         NodeStmtVar *stmt_var = malloc(sizeof(NodeStmtVar));
         stmt_var->ident = ident_token;
@@ -251,9 +257,14 @@ NodeProg *parse_prog(Token **tokens, int token_count) {
     prog->stmts = NULL;
     prog->stmt_count = 0;
 
-    int token_pos = 0;
-    while (token_pos < token_count) {
-        NodeStmt *stmt = parse_stmt(tokens, &token_pos, token_count);
+    ParserCtx ctx = {
+        .tokens = *tokens,
+        .current_pos = 0,
+        .token_count = token_count
+    };
+
+    while (ctx.current_pos < token_count) {
+        NodeStmt *stmt = parse_stmt(&ctx);
         prog->stmts = realloc(prog->stmts, sizeof(NodeStmt*) * (prog->stmt_count + 1));
         prog->stmts[prog->stmt_count] = stmt;
         prog->stmt_count++;
